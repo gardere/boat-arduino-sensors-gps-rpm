2a90a65050056321a66921ebcda90d0fbf7b622b
Mathieu Gardere <wam@mathieugardere.com>
2018-05-27 18:14:17 +1000

    updated sensor reading to use variable voltage input - switched to 3.3v instead of 5v

--- reader_v0.02/sensor_readings.cpp
+++ reader_v0.02/sensor_readings.cpp
@@ -1,5 +1,5 @@
-#include "Arduino.h"
 #include "sensor_readings.h"
+#include "Arduino.h"
 
 
 int voltagePin = A1;
@@ -7,17 +7,18 @@ float TOP_RESISTOR_VALUE = 110.0f; //aka R1  in voltage divider equation
 int sensorPins[4] = { A2, A3, A4, A5 };
 int numberOfSensors = sizeof(sensorPins) / sizeof(sensorPins[0]);
 
+float fullVoltage = 3.2f;
+
 
 float getSensorVoltage(int pinNumber) {
    float vout = analogRead(pinNumber);
-   return vout * 3.2f; //we are returning Vin, the input voltage before going through the divider
+   return vout / 1024.0f * (1000 + 2200) / 1000 * fullVoltage; //we are returning Vin, the input voltage before going through the divider
 }
 
-float getSensorResistance(int pinNumber) {
-  //TODO: apply voltage divider formula to output a result in ohms
+unsigned int getSensorResistance(int pinNumber) {
+  float rawReading = analogRead(pinNumber) / 1024.0f * 5.0f; //aka Vout  in voltage divider equation - arduino analog inputs readings return a number of (5V / 1024 'steps')
 
-  float rawReading = analogRead(pinNumber); //aka Vout  in voltage divider equation
-  return rawReading * TOP_RESISTOR_VALUE  / (5.0f - rawReading); // we assume Vin is divided by a stable and precise 5V Arduino output
+  return (unsigned int)(rawReading * TOP_RESISTOR_VALUE / (fullVoltage - rawReading)); // we assume Vin is divided by a stable and precise Arduino voltage output
 }
 
 void sendSensorsData() {
