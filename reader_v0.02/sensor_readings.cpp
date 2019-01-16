ba0eca9898647976516423beae188529de5eb8dc
Mathieu Gardere <wam@mathieugardere.com>
2019-01-17 07:54:53 +1100

    fix - readings conversions

--- reader_v0.02/sensor_readings.cpp
+++ reader_v0.02/sensor_readings.cpp
@@ -7,7 +7,7 @@ float TOP_RESISTOR_VALUE = 110.0f; //aka R1  in voltage divider equation
 int sensorPins[4] = { A1, A2, A3, A4 };
 int numberOfSensors = sizeof(sensorPins) / sizeof(sensorPins[0]);
 
-float fullVoltage = 3.42f; //Best result after calibration
+float fullVoltage = 3.32f; //Best result after calibration
 
 
 float getSensorVoltage(int pinNumber) {
@@ -16,7 +16,7 @@ float getSensorVoltage(int pinNumber) {
 }
 
 unsigned int getSensorResistance(int pinNumber) {
-  float rawReading = analogRead(pinNumber) / 1024.0f * 5.0f; //aka Vout  in voltage divider equation - arduino analog inputs readings return a number of (5V / 1024 'steps')
+  float rawReading = analogRead(pinNumber) / 1024.0f * fullVoltage; //aka Vout  in voltage divider equation - arduino analog inputs readings return a number of (5V / 1024 'steps')
 
   return (unsigned int)(rawReading * TOP_RESISTOR_VALUE / (fullVoltage - rawReading)); // we assume Vin is divided by a stable and precise Arduino voltage output
 }
@@ -34,7 +34,7 @@ void sendSensorsData() {
 
 void sendRawReadings() {
   Serial.print("RAW ");
-  Serial.print(analogRead(A1));
+  Serial.print(analogRead(voltagePin));
   Serial.print(" ");
   for(int i=0; i<numberOfSensors; ++i) {
     Serial.print(analogRead(sensorPins[i]));
